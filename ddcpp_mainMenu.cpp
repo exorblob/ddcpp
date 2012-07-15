ffe3ddca7976e99ee8c5cc772f133698a04dd4e2
exorblob <garethdavis9@gmail.com>
2012-07-15 17:09:15 -0300

    second commit

---
+++ ddcpp_mainMenu.cpp
@@ -0,0 +1,27 @@
+#include "ddcpp.h"
+
+int MainMenu::GetUserOption()
+{
+	UserInput UserInput;
+	
+	bool Correct = false;
+	
+	int Choice = 0;
+	
+	while (Correct == false)
+	{
+		cout << "Select the option you would like by typing the corresponding number and pressing enter" << endl;
+		
+		cout << "1. Generate new character" << endl
+			  << "2. Edit existing character" << endl
+			  << "3. Print character sheet" << endl
+			  << "4. delete character" << endl
+			  << "5. exit the program" << endl;			  	  
+			  
+		Choice = UserInput.GetInt(5);	
+		
+		Correct = UserInput.ValidateInput(Choice);
+	}
+	
+	return Choice;
+}
